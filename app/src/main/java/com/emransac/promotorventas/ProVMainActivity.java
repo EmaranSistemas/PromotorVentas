60755a2887b081bd2ae2f120685ff614980a85ec
EmaranSistemas <sistemas.emaransac@gmail.com>
2023-06-16 17:07:27 -0500

    Initial commit

---
+++ app/src/main/java/com/emransac/promotorventas/ProVMainActivity.java
@@ -0,0 +1,14 @@
+package com.emransac.promotorventas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProVMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pro_vmain);
+    }
+}
